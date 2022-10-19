e680ac34bf49c98a9b15e6175df23c9d62b09206
Carlos Fisac Ferrández <charles8ff@gmail.com>
2022-10-19 15:58:30 +0200

    Add new file from practice m1

---
+++ polynoms.cpp
@@ -0,0 +1,38 @@
+// //Interface for a sample of real numbers
+// interface iSample {
+// //return the maximum number in the sample
+// public double getMax();
+// //return the minimum number in the sample
+// public double getMin();
+// //return the sum of all the numbers in the sample
+// public double sum();
+// //return the mean of the sample
+// public double mean();
+// //return the mean of the sample
+// public double median()
+//}
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class iPolynomial
+{
+private:
+    /* data */
+    int degree;
+    int nterms;
+    string polynom;
+public:
+    iPolynomial(/* args */);//sample constructor
+    ~iPolynomial();//sample destructor
+};
+
+iPolynomial::iPolynomial(/* args */)
+{
+}
+
+iPolynomial::~iPolynomial()
+{
+}
+
