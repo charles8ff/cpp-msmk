6d0cbfe33fa7e62113ec41e44caef83b81b3c513
Carlos Fisac Ferrández <charles8ff@gmail.com>
2022-11-16 16:04:45 +0100

    Terniary

--- strings.cpp
+++ strings.cpp
@@ -15,6 +15,17 @@ int main(){
     cout << str3 << endl;
     cout << str4 << endl;
 
+    string ñññ = "123456789012345678901234567890123";
 
+    cout << ñññ.length() << endl;
+    cout << ñññ.size() << endl;
+
+    if (ñññ.length()>12){
+        cout << "es segura" << endl;
+    } else {
+        cout << "NO es segura" << endl;
+    }
+
+    ñññ.length()>12 ? cout << "es segura" << endl : cout << "NO es segura" << endl;
     return 0;
 }
