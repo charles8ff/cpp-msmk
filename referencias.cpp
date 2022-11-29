834b183495def61676659a99f59777d4648cf66f
Carlos Fisac Ferrández <charles8ff@gmail.com>
2022-11-29 17:54:22 +0100

    Lunch & Dinner

--- referencias.cpp
+++ referencias.cpp
@@ -16,19 +16,30 @@ int main(){
     string comida = "pizza";
     cout << &comida << endl; // Esto devuelve una dirección de memoria
                             // del estilo 0x53c69ffcf0 <- eso es hexadecimal
+                            // // dIcE CarLoS qUe esTá RePeTidO mimimimi
     // Nosotros podemos, guardar esa posición en un PUNTERO
-    string *ptr = &comida;
+    string* ptr = &comida;  // Se puede escribir también como:
+                            // string *ptr o string * ptr
+                            // Pero no son recomendables
 
-    cout << "Veamos qué es comida: \n" ;
+    cout << "\nVeamos qué es comida: \n" ;
     cout << "* Comida es: " << comida << endl; // pizza
     cout << "* Comida es: " << &comida << endl;// dirección
     cout << "* Comida es: " << ptr << endl;    // también dirección
     cout << "* Comida es: " << *ptr << endl;   // también pizza
 
-    // ! Por el amor de dios miren a gabs
+    // ! Voy a cambiar todos los nombres porque si no el compilador llora que flipas
 
+    string cena = "sushi";
+    string* puntero = &cena;
+    cout << cena << endl; // sushi
+    cout << *puntero << endl; // sushi
 
-
+    // Ahora vamos a desreferenciar (quitar el puntero)
+    // y coseguimos cambiar cena sin tocarla.
+    *puntero = "Hamburgesa";
+    cout << cena << endl; // Hamburgesa
+    cout << *puntero << endl; // Hamburgesa
 
     return 0;
 }
