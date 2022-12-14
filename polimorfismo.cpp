f7fc1efce1fd999906dff94db6e76ae55ea3d3cf
Carlos Fisac Ferrández <charles8ff@gmail.com>
2022-12-14 17:49:39 +0100

    TODO: EDUCACIÓN

--- polimorfismo.cpp
+++ polimorfismo.cpp
@@ -16,8 +16,13 @@ using namespace std;
     // Aulas
     // Baños
 
-// Departamentos
+//! esto va a ser una interfaz
+//? Educación
+    // Asignaturas
+    // Departamentos
 
+
+// TODO: Acabar en lucidchart como creemos que tiene que ser la interfaz educación.
 class Instalación{
     protected:
 
@@ -27,13 +32,18 @@ class Instalación{
         int numeroPuertas = 1;
         string materialPuertas = "Cristal";
         int numeroVentanas = 2;
+        bool luzEncendida = false;
 
     public:
         Instalación();
+        void pulsarInterruptor(){
+            luzEncendida = !luzEncendida;
+        };
 };
 
 class Aula: public Instalación{
     private:
+
         bool tieneProyector;
         int numProyectores = 0;
         bool cámara;
@@ -46,8 +56,12 @@ class Aula: public Instalación{
 
 class Baño: public Instalación{
     private:
+        bool unisex = false;
+        string género;
+        bool tieneSecador;
         bool quedaJabón;
         bool quedaPapel;
+        bool tienePapelera;
 
     public:
         Baño();
@@ -63,11 +77,68 @@ class Educación{
 
 class Persona{
     private:
+
+        string ojos;
+        string nombre;
+        string apellidos;
+        string DNI;
+        string teléfono;
         int edad = 17;
 
     public:
         Persona();
 };
+
+class Profesor: public Persona{
+    private:
+
+        float nómina = 1000.00;
+        int numeroAsignaturas;
+        float tasaÉxito;
+        string numSeguridadSocial;
+        bool tieneSeguro;
+        int dietas;
+
+    public:
+        Profesor();
+        void corregir(){};
+        void evaluar(){};
+        void impartirClase( string asignatura){};
+        void pasarLista(){};
+
+};
+
+class Alumno : public Persona{
+    private:
+        string grado;
+        string curso;
+        string númeroLista;
+        float notas[4] = {0.0,0.0,0.0,0.0};
+        float porcentajeAsistencia = 00.00;
+        int entregados;
+        int créditosCursados;
+        int créditosPendientes;
+
+    public:
+
+        void setGrado( string g ){
+            grado = g;
+        }
+
+        string getGrado(){
+            return grado;
+        }
+        void estudiar(){};
+        void asistir(){};
+        void programar(){};
+        void jakiar(){};
+        void hablar(){};
+        void aprobar(){};
+        void suspender(){};
+        void hacerPrácticas(){};
+
+};
+
 
 int main (){
 
