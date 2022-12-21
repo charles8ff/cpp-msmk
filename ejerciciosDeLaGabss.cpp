02e0eb8ebc262782eecdc958a3f2dc10cec1d4d3
Carlos Fisac Ferrández <charles8ff@gmail.com>
2022-12-21 17:28:55 +0100

    Más cosas

--- ejerciciosDeLaGabss.cpp
+++ ejerciciosDeLaGabss.cpp
@@ -94,12 +94,89 @@ int main(){
     if (num1 <= num2) cout << "No acontece" << endl; // Os fiáis de que está escrito < = sin espacio
 
     if (num1 == 6 && num2 == 2) cout << "Chachi" <<endl;
+    if (num1 == 6 || num2 == 2) cout << "Chachi" <<endl;
 
     if (!(num1 < 5 && num2 < 10)){
             cout << "Pasó algo" <<endl;
+        } else if (num1 == 6) {
+            cout << "Pasó más" <<endl;
         } else {
-            cout << "Pasó otra cosa" <<endl; // No sucede, porque num1 es 6
+            cout << "Pasó otra cosa" <<endl;
         }
+
+    int time = 20;
+    // * Ternarias:
+    string result = (time < 18) ? "Good day." : "Good evening.";
+    cout << result;
+
+    // Librería math : existe
+    // A otra cosa
+
+
+    // Switches
+    int caso = 0;
+    switch (caso){
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    // Repetir al gusto
+
+    default: // Entra en el default si no encuentra ninguno de los casos
+        /* code */
+        break;
+    }
+
+
+    // Bucles
+    int i = 10;
+    while (i > 0) {
+        cout << "Hola";
+        i--;
+    }
+
+    do {
+        cout << "Hola";
+        i++;
+    } while (i < 10);
+
+    for (int j = 0; j < 5; j = j+2) {
+        cout << "salu2";
+        // Aquí ya no hace falta el j=j+2 porque está arriba
+    }
+    // ! Los pasos prohibidos:
+    // break;
+    // continue;
+    for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+            continue;
+        }
+        cout << i << "\n";
+    }
+    cout << "\n----------------------------------\n"; // Aquí saca todos menos el 4
+    for (int i = 0; i < 10; i++) {
+        if (i == 4) {
+            break;
+        }
+        cout << i << "\n";
+    } // Aquí parará en el 4 y hastaluegomaricarmen
+
+
+    // Arrays
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    cout << cars[0]; // Efectivamente imprime Volvo porque empieza en 0
+
+    // Para recorrer un array
+    for (int i = 0; i < 4; i++){
+        cout << "La i vale: " << i <<" "<<endl;
+        cout << "Y el coche es : "<< cars[i] << endl;
+    }
+    // ! Siempre inicializar los arrays con el número de elementos que va a tener
 
     return 0;
 }
