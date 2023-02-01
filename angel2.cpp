3423d9d5b216e6bc99fd27491eaec66dc657ab37
Carlos Fisac Ferrández <charles8ff@gmail.com>
2023-02-01 17:50:12 +0100

    c

--- angel2.cpp
+++ angel2.cpp
@@ -3,13 +3,49 @@
 #include <cmath>
 
 using namespace std;
+class Clase {
+    public:
+        Clase(){};
+        void acción(){
+            cout << "Aaaaaaacción" <<endl;
+        }
+};
 
+int main(){
 
-int main () {
+    int i = 1;
 
-    int i = 0;
+    if (i <= 0) {
 
-    cout << "XD" <<endl;
+        Clase miClase;
+        cout << "if 1" <<endl;
+
+    } else {
+
+        if(i == 1) {
+
+            //miClase.acción(); //! NOT DECLARED
+            cout << "if 1" <<endl;
+
+        } else {
+            if(i == 2) {
+
+                cout << "if 2" <<endl;
+
+            } else {
+                if(i == 3) {
+
+                    cout << "if 3" <<endl;
+
+                } else {
+                    if(i == 4) {
+
+                        cout << "if 4" <<endl;
+
+                    }
+                }
+            }
+        }
+    }
     return 0;
-
 }
